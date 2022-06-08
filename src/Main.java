e2ac04cfd793ac213f72ef63dc89137a3083ce63
SQQ86 <dzamyhov.artur@gmail.com>
2022-06-08 20:21:03 +0300

    Release

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        int buf;
-        ArrayList list = new ArrayList<>();
+        Scanner scan = new Scanner(System.in); //читаем с консоли
+        int a = scan.nextInt();// сохраняем число
+        int buf;//буферная переменная хранит остаток от деления на 10
+        ArrayList list = new ArrayList<>();// создаем List
         while (a!=0){
             buf = a%10;
-            list.add(buf);
-            a = a/10;
+            list.add(buf);//заносим в List каждую цифру по отдельности
+            a = a/10; //цикл продолжается, пока а не будет равно 0(пока все цифры из числа не будут занесены в List),
         }
-        Collections.sort(list);
-        System.out.println(list.get(list.size()-1));
+        Collections.sort(list); //сортируем по взрастанию
+        System.out.println(list.get(list.size()-1));//выводим цифру, последнюю в List , так как она является наибольшей
     }
 }
